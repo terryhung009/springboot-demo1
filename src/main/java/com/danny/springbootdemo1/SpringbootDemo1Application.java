d07418353a233f1cbb7b7b04ac18a9642ff29555
terryhung009 <76043300+terryhung009@users.noreply.github.com>
2022-10-07 16:22:45 +0800

    1

--- src/main/java/com/danny/springbootdemo1/SpringbootDemo1Application.java
+++ src/main/java/com/danny/springbootdemo1/SpringbootDemo1Application.java
@@ -1,7 +1,10 @@
 package com.danny.springbootdemo1;
 
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootDemo1Application {
@@ -9,5 +12,18 @@ public class SpringbootDemo1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDemo1Application.class, args);
 	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx){
+		return args-> {
+			
+		}
+
+
+	}
+
+
+
+
 
 }
